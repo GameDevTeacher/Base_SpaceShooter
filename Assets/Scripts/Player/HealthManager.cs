983c04c327c114a33784dc85e830c3c68a870189
Markus Lange <markus@gamedevteacher.com>
2021-04-27 13:48:26 +0200

    Added health

--- Assets/Scripts/Player/HealthManager.cs
+++ Assets/Scripts/Player/HealthManager.cs
@@ -1,20 +1,28 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Player
 {
     public class HealthManager : MonoBehaviour
     {
+    
+        public int lives = 3;
+        private int maxLives = 3;
         
         // Start is called before the first frame update
         void Start()
         {
-        
+            lives = maxLives;
         }
 
         // Update is called once per frame
         void Update()
         {
-        
+            if (lives == 0)
+            {
+                SceneManagement.LoadScene()
+            }
+            
         }
     }
 }
