df4241e306ca423d01855cf0b94df8305ad2bf24
Markus Lange <markus@gamedevteacher.com>
2021-04-27 14:04:13 +0200

    Added Score and Game Manager

---
+++ Assets/Scripts/Player/GameManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager: MonoBehaviour
+{
+    public void Update()
+    {
+        if (ScoreManager.score >= 10)
+        {
+            SceneManagement.LoadScene("WinScene");
+        }
+        
+        if (HealthManager.lives <= 0)
+        {
+            SceneManagement.LoadScene("MainMenu")
+        }
+    }
+    
+}
