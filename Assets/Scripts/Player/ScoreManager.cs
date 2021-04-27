80a5fd5d8bf3a4f02de434b25e579b1068050687
Markus Lange <markus@gamedevteacher.com>
2021-04-27 14:05:00 +0200

    additional

---
+++ Assets/Scripts/Player/ScoreManager.cs
@@ -0,0 +1,11 @@
+using UnityEngin;
+
+public class ScoreManager: MonoBehaviour
+{
+    public static int score {get; private set;}
+    
+    public void AddScore()
+    {
+        score++;
+    }
+}
